2230c09f8128862649f98274d61280cf2cba2024
marysol <marilirulita@gmail.com>
2022-02-16 14:59:32 -0800

    Add test to header

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer';
 import Header from '../functionBased/Header';
 import InputTodo from '../functionBased/InputTodo';
 import TodoContainer from '../functionBased/TodoContainer';
+import TodosList from '../functionBased/TodosList'
 //import TodoItem from '../functionBased/TodoItem';
 
 it('renders correctly', () => {
@@ -20,15 +21,19 @@ it('renders correctly', () => {
 });
 
 it('renders correctly', () => {
+  
   const tree = renderer
     .create(<InputTodo></InputTodo>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-// it('renders correctly', () => {
-//   const tree = renderer
-//     .create(<TodoItem></TodoItem>)
-//     .toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<TodosList todos={todos}
+      handleChange={handleChange}
+      handleDelete={handleDelete}
+      handleEdit={handleEdit}></TodosList>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
