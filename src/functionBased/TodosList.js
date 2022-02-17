0e21757cb77b4a7da3d4172708f26d4e850f6a52
marysol <marilirulita@gmail.com>
2022-02-16 19:33:10 -0800

    Change todolist class to function

--- src/functionBased/TodosList.js
+++ src/functionBased/TodosList.js
@@ -1,22 +1,20 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-class TodosList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((elem) => (
-          <TodoItem
-            key={elem.id}
-            todo={elem}
-            handleChange={this.props.handleChange}
-            handleDelete={this.props.handleDelete}
-            handleEdit={this.props.handleEdit}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const TodosList = (props) => {
+  return (
+    <ul>
+      {props.todos.map((elem) => (
+        <TodoItem
+          key={elem.id}
+          todo={elem}
+          handleChange={props.handleChange}
+          handleDelete={props.handleDelete}
+          handleEdit={props.handleEdit}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default TodosList;
