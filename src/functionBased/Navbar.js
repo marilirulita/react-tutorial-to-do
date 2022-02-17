bbb35d1d3b76ca1db91bac1c50ff26ed8347c4e2
marysol <marilirulita@gmail.com>
2022-02-16 21:36:30 -0800

    Update routes

--- src/functionBased/Navbar.js
+++ src/functionBased/Navbar.js
@@ -1,6 +1,32 @@
-import React from "react"
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Hello from Navbar</div>
-}
-export default Navbar
+  const links = [
+    {
+      id: 1,
+      path: "/",
+      text: "Home",
+    },
+    {
+      id: 2,
+      path: "/about",
+      text: "About",
+    },
+  ];
+  return (
+    <nav className="navBar">
+      <ul>
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <NavLink to={link.path} activeClassName="active-link" exact>{link.text}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
